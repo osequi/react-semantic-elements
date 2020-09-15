b6b8dd2a9d72cd44abb01e5ea20cd1ebb2362144
osequi <cs@osequi.com>
2020-09-15 20:40:12 +0300

    Working on the nav element

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,23 +4,57 @@ import PropTypes from "prop-types";
 /**
  * Defines the prop types
  */
-const propTypes = {};
+const propTypes = {
+  /**
+   * The element title.
+   * Usually it's not displayed on the UI but in the HTML code helping out the W3C .
+   * @type {string}
+   */
+  title: PropTypes.string,
+  /**
+   * Display the element title?
+   * Set to false by default
+   * @type {bool}
+   */
+  displayTitle: PropTypes.bool,
+  /**
+   * The content of the element.
+   * @type {any}
+   */
+  children: PropTypes.any,
+  /**
+   * The title tag.
+   * Set to `<h3>` by default
+   * @type {element}
+   */
+  TitleTag: PropTypes.node,
+};
 
 /**
  * Defines the default props
  */
-const defaultProps = {};
+const defaultProps = {
+  title: null,
+  displayTitle: false,
+  children: null,
+  TitleTag: "<h3>",
+};
 
 /**
- * Displays the component
+ * Displays a `<nav>` component
  */
 const Nav = (props) => {
-  const { title, display, children } = props;
+  const { title, displayTitle, children, TitleTag } = props;
 
-  const displayAttribute = display ? null : "hidden";
-  const titleElement = title ? (
-    <TitleTag displayAttribute>{title}</TitleTag>
-  ) : null;
+  /**
+   * Displays nothing if there is no `children` prop defined
+   */
+  if (!children) return null;
+
+  /**
+   * Displays the title tag if the `title` attribute is set
+   */
+  const titleElement = title ? <h3 hidden={!displayTitle}>{title}</h3> : null;
 
   return (
     <nav>
--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -8,4 +8,25 @@ export default {
   component: Nav,
 };
 
+/**
+ * Should display nothing since `children` is not set
+ */
 export const Default = () => <Nav />;
+Default.decorators = [
+  (Story) => (
+    <div>
+      <p>It should be empty because `children` is not set by default.</p>
+      <Story />
+    </div>
+  ),
+];
+
+export const WithChildren = () => (
+  <Nav children="Please check the `HTML` tab on the right sidebar for the generated code." />
+);
+
+export const WithTitle = () => <Nav children="children" title="title" />;
+
+export const WithTitleDisplayed = () => (
+  <Nav children="children" title="title" displayTitle={true} />
+);
