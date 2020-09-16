93152c8b9bd3f506142d9ec0035dce9139ca4240
osequi <cs@osequi.com>
2020-09-16 11:21:12 +0300

    Ready to test and refactor

--- src/components/Article/Article.stories.js
+++ src/components/Article/Article.stories.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
 
 import Article from "./Article";
 import description from "./Article.md";
 
-storiesOf("Article", module).add("Overview", () => <Article />, {
-  notes: { markdown: description }
-});
+export default {
+  title: "Components/Article",
+  component: Article,
+};
+
+/**
+ * Should display nothing since `children` is not set
+ */
+export const Default = () => <Article />;
+Default.decorators = [
+  (Story) => (
+    <div>
+      <p>It should be empty because `children` is not set by default.</p>
+      <Story />
+    </div>
+  ),
+];
--- src/components/Demo/Demo.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import Demo from "./Demo";
-import description from "./Demo.md";
-
-storiesOf("Demo", module).add("Overview", () => <Demo />, {
-  notes: { markdown: description }
-});
