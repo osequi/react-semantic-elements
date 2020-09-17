583ca7fe569f837c64d54a1291af02447ea2066c
osequi <cs@osequi.com>
2020-09-17 09:09:15 +0300

    Adding demo

--- src/components/Demo2/Demo2.js
+++ src/components/Demo2/Demo2.js
@@ -5,7 +5,15 @@ import styled from "styled-components";
 /**
  * Imports other components and hooks
  */
-import { Article } from "../ReactSemanticElements";
+import {
+  Section,
+  Article,
+  Aside,
+  Header,
+  Footer,
+  Nav,
+  Headings,
+} from "../ReactSemanticElements";
 
 /**
  * Defines the prop types
@@ -20,15 +28,61 @@ const defaultProps = {};
 /**
  * Styles the component container
  */
-const Container = styled("section")((props) => ({}));
+const Container = styled(Section)((props) => ({}));
+
+/**
+ * Styles the strapline
+ */
+const Strapline = styled(Headings)((props) => ({
+  fontSize: "100%",
+}));
+
+/**
+ * Styles the menu
+ */
+const Menu = styled(Nav)((props) => ({}));
+
+/**
+ * Styles the pagination
+ */
+const Pagination = styled(Nav)((props) => ({
+  border: "1px solid",
+}));
 
 /**
  * Displays the component
  */
 const Demo2 = (props) => {
   return (
-    <Container className="Demo2">
-      <Article title="Article">Article body</Article>
+    <Container
+      className="Demo2"
+      title="React Semantic Elements"
+      heading={{ level: "h1" }}
+    >
+      <Strapline level="h2">
+        HTML semantic sectioning elements for React.
+      </Strapline>
+      <Header>
+        <Menu>Menu navigation inside a `nav`</Menu>
+      </Header>
+      <Article>
+        <p>
+          This is how this page is outlined in the{" "}
+          <a href="https://validator.w3.org/" title="W3C validator">
+            W3C validator.
+          </a>
+        </p>
+        <p>
+          <img src="/assets/images/outlined.png" alt="Outlined" />
+        </p>
+        <Aside>
+          <p>Article meta information inside an `aside`</p>
+        </Aside>
+      </Article>
+      <Pagination>Pagination inside a `nav`</Pagination>
+      <Footer>
+        <p>Footer</p>
+      </Footer>
     </Container>
   );
 };
