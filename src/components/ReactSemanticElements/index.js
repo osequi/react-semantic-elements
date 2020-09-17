bf6ea53e0456a14f5babc9aa6563328d3a7bd55f
osequi <cs@osequi.com>
2020-09-17 08:43:37 +0300

    Exporting all from ReactSemanticElements

--- src/components/ReactSemanticElements/index.js
+++ src/components/ReactSemanticElements/index.js
@@ -5,3 +5,11 @@ export {
   requiredPropsAreSet,
   nonEmptyClassname,
 } from "./ReactSemanticElements";
+
+export { Article, ArticlePropTypes, ArticleDefaultProps } from "../Article";
+export { Aside, AsidePropTypes, AsideDefaultProps } from "../Aside";
+export { Footer, FooterPropTypes, FooterDefaultProps } from "../Footer";
+export { Header, HeaderPropTypes, HeaderDefaultProps } from "../Header";
+export { Headings, HeadingsPropTypes, HeadingsDefaultProps } from "../Headings";
+export { Nav, NavPropTypes, NavDefaultProps } from "../Nav";
+export { Section, SectionPropTypes, SectionDefaultProps } from "../Section";
